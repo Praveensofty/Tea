8c9b530fb68e38ba85ca43a717c4d1c2a8698f98
X735542 <praveenkumar.x1.paramasivam@kp.org>
2024-12-22 11:30:12 -0700

    Initial commit: Added tea brand project with Razor Pages and PostgreSQL

---
+++ Library/Helpers/TeaDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("TeaDetails", Schema = "public")]
+    public class TeaDetail
+    {
+        [Key]
+        [Column("ID")]
+        public long TeaId { get; set; }
+
+        [Required]
+        [Column("Name", TypeName = "character varying(100)")]
+        public string TeaName { get; set; }
+    }
---
+++ Library/Helpers/TeaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class Tea
+	{
+		   public long TEA_ID { get; set; } // Maps to the primary key
+            public string TEA_NAME { get; set; } // Book name
+        
+	}
+}
